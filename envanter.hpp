6b615b8ce982e77ee322081064eb2eb23424339a
tyson <65432197+TYSON-Alii@users.noreply.github.com>
2022-04-11 17:24:37 +0300

    Update envanter.hpp

--- envanter.hpp
+++ envanter.hpp
@@ -1,40 +1,44 @@
+#include <iostream>
 #include <array>
-template <typename enum_type, int n, enum_type empty_item>
+template <typename enum_type, size_t n, enum_type empty_item = (enum_type)0>
 class envanter {
 private:
-    typedef pair<enum_type, size_t> item_t;
-    std::array<item_t, n> data;
+    using uint = unsigned int;
+    struct item_t {
+        enum_type item = empty_item;
+        uint count = 0;
+    };
 public:
-    inline auto& Data() { return data; };
-    envanter() { std::fill(data.begin(), data.end(), make_pair(empty_item, 0)); };
-    inline struct {
-        enum_type item;
-        int count;
-    } operator[](int index) { return {data[index].first(), data[index].seconds()}; };
-    int Push(enum_type item) {
-        auto f = find_if(data.begin(), data.end(), [&](const item_t& i) -> bool { return i.first == item; });
+    std::array<item_t, n> data;
+    envanter() { std::fill(data.begin(), data.end(), item_t{ empty_item, 0 }); };
+    inline item_t& operator[](const uint& index) { return data[index]; };
+    uint push(enum_type item, const uint& count = 1u) {
+        auto f = find_if(data.begin(), data.end(), [&](const item_t& i) -> bool { return i.item == item; });
         if (f != data.end()) {
-            f->second++;
+            f->count += count;
             return std::distance(data.begin(), f);
         }
         else {
-            auto f = find_if(data.begin(), data.end(), [&](const item_t& i) -> bool { return i.first == empty_item; });
+            auto f = find_if(data.begin(), data.end(), [&](const item_t& i) -> bool { return i.item == empty_item; });
             if (f != data.end()) {
-                *f = make_pair(item, 1);
+                f->item = item;
+                f->count = count;
                 return std::distance(data.begin(), f);
             };
         };
         return -1;
     };
-    bool Pop(int index) {
-        if (data[index].second != 0) {
-            data[index].second--;
-            if (data[index].second == 0)
-                data[index].first = empty_item;
+    bool pop(const uint& index, const uint& count = 1u) {
+        if (data[index].count != 0u) {
+            if (data[index].count > count)
+                data[index].count -= count;
+            else
+                data[index].count = 0u;
+            if (data[index].count == 0u) data[index].item = empty_item;
             return true;
-        }
+        };
         return false;
     };
-    void Swap(int index1, int index2) { std::swap(data[index1], data[index2]); };
-    void Remove(int index) { data[index] = make_pair(empty_item,0); };
+    void swap(const uint& index1, const uint& index2) { std::swap(data[index1], data[index2]); };
+    void remove(const uint& index) { data[index] = { empty_item, 0 }; };
 };
